629a5f13e963e92ab8fa43a39c6fa5b7756ccdcb
Nisiman <nisimanbhardwaj29@gmail.com>
2024-01-16 14:04:22 +0000

    actually shifted theme icon to left of hamburger on phone

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -33,50 +33,55 @@ export default function Navbar() {
   ];
 
   return (
-    <div className="flex justify-between items-center w-full h-20 px-4 text-white nav">
-      <ul className="hidden md:flex">
-        {links.map((link: any, index: number) => (
-          <li
-            key={index}
-            className={`${
-              pathname === link.link
-                ? "underline text-black dark:text-white scale-105"
-                : "text-gray-700 dark:text-gray-400"
-            } nav-links px-4 cursor-pointer text-md hover:scale-105 hover:text-black dark:hover:text-white duration-200`}
-          >
-            <Link href={link.link}>{link.name}</Link>
-          </li>
-        ))}
-      </ul>
-
-      <div
-        onClick={() => setNav(!nav)}
-        className="cursor-pointer px-4 z-20 text-black dark:text-white md:hidden"
-      >
-        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
-      </div>
-
-      {nav && (
-        <ul className="z-10 flex flex-col justify-center items-center fixed top-0 left-0 w-full h-screen bg-sky-300 dark:bg-slate-900 text-black dark:text-white">
+    <>
+      <div className="flex justify-between items-center w-full h-20 px-4 text-white nav">
+        <div className="md:hidden">
+          <ThemeSwitch />
+        </div>
+        <ul className="hidden md:flex">
           {links.map((link: any, index: number) => (
             <li
               key={index}
               className={`${
                 pathname === link.link
-                  ? "underline text-black dark:text-white"
-                  : "text-gray-800 dark:text-gray-400"
-              } px-4 cursor-pointer py-6 text-2xl`}
+                  ? "underline text-black dark:text-white scale-105"
+                  : "text-gray-700 dark:text-gray-400"
+              } nav-links px-4 cursor-pointer text-md hover:scale-105 hover:text-black dark:hover:text-white duration-200`}
             >
-              <Link onClick={() => setNav(!nav)} href={link.link}>
-                {link.name}
-              </Link>
+              <Link href={link.link}>{link.name}</Link>
             </li>
           ))}
         </ul>
-      )}
-      <div>
-        <ThemeSwitch />
+
+        <div
+          onClick={() => setNav(!nav)}
+          className="cursor-pointer px-2 z-20 text-black dark:text-white md:hidden"
+        >
+          {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+        </div>
+
+        {nav && (
+          <ul className="z-10 flex flex-col justify-center items-center fixed top-0 left-0 w-full h-screen bg-sky-300 dark:bg-slate-900 text-black dark:text-white">
+            {links.map((link: any, index: number) => (
+              <li
+                key={index}
+                className={`${
+                  pathname === link.link
+                    ? "underline text-black dark:text-white"
+                    : "text-gray-800 dark:text-gray-400"
+                } px-4 cursor-pointer py-6 text-2xl`}
+              >
+                <Link onClick={() => setNav(!nav)} href={link.link}>
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+        <div className="hidden md:block">
+          <ThemeSwitch />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
