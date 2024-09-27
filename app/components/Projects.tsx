97565a4cfb97f08a0ac5c3988eccb44bd3a49120
Nisiman <nisimanbhardwaj29@gmail.com>
2024-09-27 11:25:57 +0100

    adjusting projects initial height

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -35,7 +35,7 @@ export default function Projects() {
     const div = projectsContainerRef.current;
     if (!div) return;
     if (!isExpanded) {
-      div.style.maxHeight = "5000px";
+      div.style.maxHeight = "4500px";
     } else {
       div.style.maxHeight = initialMaxHeight;
     }
