c833c9f78b708179036c0fd8a3ae3b63ac7ba21f
Nisiman <nisimanbhardwaj29@gmail.com>
2024-09-29 01:33:57 +0100

    music design changes

--- app/components/MusicList.tsx
+++ app/components/MusicList.tsx
@@ -6,7 +6,7 @@ const renderRelevantRow = (item: Track | Artist, type: string) => {
   if (type === "artist") {
     const artist = item as Artist;
     return (
-      <h4 className="text-sm lg:text-lg line-clamp-1 justify-self-center">
+      <h4 className="text-sm md:text-lg lg:text-xl line-clamp-1 justify-self-center">
         {artist.name}
       </h4>
     );
@@ -14,7 +14,7 @@ const renderRelevantRow = (item: Track | Artist, type: string) => {
     const track = item as Track;
     return (
       <>
-        <h4 className="text-sm lg:text-lg line-clamp-1">{track.title}</h4>
+        <h4 className="text-sm md:text-lg line-clamp-1">{track.title}</h4>
         <h5 className="text-xs line-clamp-1">{track.artist}</h5>
       </>
     );
@@ -31,8 +31,8 @@ export default function MusicList({ type, list }: MusicListProps) {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <div className="w-full flex items-center gap-4 h-[68px] px-2 rounded-sm font-semibold group hover:bg-green-200 dark:hover:bg-green-900 hover:bg-opacity-30 dark:hover:bg-opacity-20 hover:text-green-600 dark:hover:text-green-500">
-            <div className="flex items-center gap-1 shrink-0">
+          <div className="w-full h-[68px] flex items-center gap-2 md:gap-4 md:px-2 rounded-sm font-semibold group hover:bg-green-200 dark:hover:bg-green-900 hover:bg-opacity-30 dark:hover:bg-opacity-20 hover:text-green-600 dark:hover:text-green-500">
+            <div className="flex items-center gap-2 shrink-0">
               <span className="text-lg select-none w-6 text-right">
                 {index + 1}.
               </span>
--- app/components/MusicWithTabs.tsx
+++ app/components/MusicWithTabs.tsx
@@ -26,7 +26,7 @@ export default function MusicWithTabs({
   topArtists,
 }: MusicWithTabsProps) {
   const [activeTab, setActiveTab] = useState("recent");
-  const tabsRef = useRef<(HTMLElement | null)[]>([]);
+  const tabsRef = useRef<HTMLButtonElement[]>([]);
   const [tabUnderlineWidth, setTabUnderlineWidth] = useState(0);
   const [tabUnderlineLeft, setTabUnderlineLeft] = useState(0);
 
@@ -35,7 +35,7 @@ export default function MusicWithTabs({
     if (activeTabIndex === -1) return;
 
     const setTabPosition = () => {
-      const currentTab = tabsRef.current[activeTabIndex] as HTMLElement;
+      const currentTab = tabsRef.current[activeTabIndex] as HTMLButtonElement;
       setTabUnderlineLeft(currentTab?.offsetLeft ?? 0);
       setTabUnderlineWidth(currentTab?.clientWidth ?? 0);
     };
@@ -69,7 +69,7 @@ export default function MusicWithTabs({
   return (
     <>
       <div className="w-full md:px-2 flex flex-col items-center my-3">
-        <div className="flew-row relative mx-auto flex h-12 rounded-3xl border border-white/40 bg-slate-300 dark:border-black/40 dark:bg-neutral-800 px-2 backdrop-blur-sm">
+        <div className="flex flew-row relative mx-auto h-12 rounded-3xl border border-white/40 bg-slate-300 dark:border-black/40 dark:bg-neutral-800 px-2 backdrop-blur-sm">
           <span
             className="absolute bottom-0 top-0 -z-10 flex overflow-hidden rounded-3xl py-1 transition-all duration-300"
             style={{ left: tabUnderlineLeft, width: tabUnderlineWidth }}
@@ -83,13 +83,14 @@ export default function MusicWithTabs({
               <button
                 key={index}
                 ref={(el) => {
+                  if (!el) return;
                   tabsRef.current[index] = el;
                 }}
                 className={`${
                   isActive
                     ? `text-white`
-                    : `hover:text-neutral-600 dark:hover:text-neutral-300`
-                } my-auto cursor-pointer select-none rounded-full px-4 text-center font-light text-xs md:text-md md:font-semibold transition duration-200`}
+                    : `hover:text-green-800 dark:hover:text-green-500`
+                } flex-1 sm:min-w-32 my-auto cursor-pointer select-none rounded-full px-4 py-2 text-center font-semibold text-xs md:text-md transition duration-200`}
                 onClick={() => setActiveTab(tab.id)}
               >
                 {tab.name}
